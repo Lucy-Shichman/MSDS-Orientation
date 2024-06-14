2de7563b26f8b6f070b986535c839241b8284936
Lucy-Shichman <lgs4zj@virginia.edu>
2024-06-14 14:29:18 -0400

    more euler problem work

--- project_euler.py
+++ project_euler.py
@@ -5,6 +5,24 @@ x=0
 for i in range (1,1000):
     if i%3==0 or i%5==0:
         x+=i
-print(x) # solution: sum= 233,168
+print(x) # solution: 233,168
 
 # Problem 2
+sum=0
+fib1=1
+fib2=1
+while fib1+fib2<4000000:
+    fib_next=fib1+fib2
+    fib1=fib2
+    fib2=fib_next
+
+    if fib2 % 2==0:
+         sum+= fib2
+print(sum) # solution: 4613732
+
+# reference: https://www.grae.io/post/euler_problem_2/
+
+# Problem 3
+factors=[]
+
+
