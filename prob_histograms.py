508bd1a54ca9d24fdc01e8f67a02ca02e1265a4d
Lucy-Shichman <lgs4zj@virginia.edu>
2024-06-15 14:06:17 -0400

    prob dist images and code

--- prob_histograms.py
+++ prob_histograms.py
@@ -14,3 +14,25 @@ poisson=np.random.poisson(5,1000)
 plt.hist(poisson)
 plt.show()
 
+# Visually Demonstrate (use histograms) to show that the 
+# Binomial and Normal distributions converge in the large N limit.
+
+normal_small = np.random.normal(0,0.1,100) # mean=0, sd=0.1, size=100
+plt.hist(normal_small,color="green")
+plt.show()
+
+binomial_small = np.random.binomial(10,0.5,100) # trials=10, prob=0.5, tested=100) 
+plt.hist(binomial_small, color="green")
+plt.show()
+
+normal_large = np.random.normal(0,0.1,10000)
+plt.hist(normal_large,color="red")
+plt.show()
+
+binomial_large = np.random.binomial(10000,0.5,100)
+plt.hist(binomial_large,color="red")
+plt.show()
+
+# "The central limit theorem (CLT) states that as the number of trials
+# (\(n\)) increases, a binomial distribution with a probability of success 
+# (\(p\)) gets closer to a normal distribution."
